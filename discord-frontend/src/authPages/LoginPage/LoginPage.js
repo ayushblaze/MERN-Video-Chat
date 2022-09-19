1acadac42c7904db4eb84a7fe6b3b78a17c0a510
Ayush Singh <ayushmoca@gmail.com>
2022-09-19 07:54:18 +0530

    Created Custom Inputs

--- discord-frontend/src/authPages/LoginPage/LoginPage.js
+++ discord-frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageInputs from "./LoginPageInputs";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <AuthBox>
       <LoginPageHeader />
-      <LoginPageInputs />
+      <LoginPageInputs 
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+      />
     </AuthBox>
   );
 };
