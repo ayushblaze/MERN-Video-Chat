07d2fc6bdf5b8e72177155f3493f1b753be6314e
Ayush Singh <ayushmoca@gmail.com>
2022-09-19 08:45:48 +0530

    Login Page Footer

---
+++ discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+
+
+const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+  return (
+    <div>
+      <CustomPrimaryButton
+        label="Login"
+        additionalStyles={{ marginTop: "30px" }}
+        disabled={!isFormValid}
+        onClick={handleLogin}
+      />
+    </div>
+  );
+};
+
+export default LoginPageFooter;
