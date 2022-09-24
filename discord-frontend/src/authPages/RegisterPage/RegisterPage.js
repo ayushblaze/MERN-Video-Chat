6cb43464e73e6aa7b6db445b115e532004033690
Ayush Singh <ayushmoca@gmail.com>
2022-09-24 20:50:20 +0530

    added register page ui

--- discord-frontend/src/authPages/RegisterPage/RegisterPage.js
+++ discord-frontend/src/authPages/RegisterPage/RegisterPage.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
 import AuthBox from "../../shared/components/AuthBox";
+import RegisterPageInputs from "./RegisterPageInputs";
+import RegisterPageFooter from "./RegisterPageFooter";
+import { validateRegisterForm } from "../../shared/utils/validators";
+
+// ! CONTINUE FROM 16
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleRegister = () => {
+    console.log("Register info:", {username, email, password});
+  };
+
+  useEffect(() => {
+    setIsFormValid(validateRegisterForm({
+      email, 
+      username,
+      password, 
+    }));
+  }, [email, username, password, setIsFormValid]);
+
   return (
     <AuthBox>
-      Register Page
+      <Typography
+        variant="h5"
+        sx={{ color: "white" }}
+      >
+        Create an account
+      </Typography>
+      <RegisterPageInputs
+        email={email}
+        setEmail={setEmail}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
+      <RegisterPageFooter 
+        handleRegister={handleRegister}
+        isFormValid={isFormValid}
+      />
     </AuthBox>
   );
 };
