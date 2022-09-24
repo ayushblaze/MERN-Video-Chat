d6e7329d7246d0cc03a43a9ba944e06ff1ea56ed
Ayush Singh <ayushmoca@gmail.com>
2022-09-24 20:49:53 +0530

    validating form inputs

--- discord-frontend/src/authPages/LoginPage/LoginPage.js
+++ discord-frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginPageFooter from "./LoginPageFooter";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageInputs from "./LoginPageInputs";
+import { validateLoginForm } from "../../shared/utils/validators";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ email, password }));
+  }, [email, password, setIsFormValid]);
 
   const handleLogin = () => {
     console.log("Logging in"); 
