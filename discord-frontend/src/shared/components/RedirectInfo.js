6ef63c715d91529dbfa9e0054c41521683c2990e
Ayush Singh <ayushmoca@gmail.com>
2022-09-24 17:36:25 +0530

    Added RedirectInfo component to shared components

--- discord-frontend/src/shared/components/RedirectInfo.js
+++ discord-frontend/src/shared/components/RedirectInfo.js
@@ -1,10 +1,24 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import { styled } from "@mui/system";
+
+const RedirectText = styled("span")({
+  color: "#00AFF4",
+  fontWeight: 500,
+  cursor: "pointer",
+});
 
 const RedirectInfo = ({ text, redirectText, additionalStyles, redirectHandler }) => {
   return (
-    <Typography>
-      
+    <Typography
+      sx={{ color: "#72767d" }}
+      style={additionalStyles ? additionalStyles : {}}
+      variant="subtitle2"
+    >
+      {text}
+      <RedirectText onClick={redirectHandler}>
+        {redirectText}
+      </RedirectText>
     </Typography>
   );
 };
