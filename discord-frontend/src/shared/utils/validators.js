f849066e2c08019c9a06979821e58d3d5c3e3d50
Ayush Singh <ayushmoca@gmail.com>
2022-09-24 20:51:05 +0530

    Validators for email, username and password

---
+++ discord-frontend/src/shared/utils/validators.js
@@ -0,0 +1,23 @@
+export const validateLoginForm = ({ email, password }) => {
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = validatePassword(password);
+
+  return isEmailValid && isPasswordValid;
+};
+
+export const validateRegisterForm = ({ email, password, username }) => {
+  return validateEmail(email) && validatePassword(password) && validateUsername(username);
+};
+
+const validateEmail = (email) => {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+};
+
+const validatePassword = (password) => {
+  return password.length > 5 && password.length < 13;
+};
+
+const validateUsername = (username) => {
+  return username.length > 2 && username.length <13;
+};
