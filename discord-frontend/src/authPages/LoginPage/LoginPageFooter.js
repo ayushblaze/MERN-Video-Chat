00c46488f508ee5b7551d3015350abee74a6352b
Ayush Singh <ayushmoca@gmail.com>
2022-09-24 18:08:36 +0530

    added handlePushToRegisterPage function

--- discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
+++ discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -1,9 +1,18 @@
 import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import { useHistory } from "react-router-dom";
 
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+  const history = useHistory();  
+
+  const handlePushToRegisterPage = () => {
+    history.push("/register");
+  };
+
   return (
+    <>
     <div>
       <CustomPrimaryButton
         label="Login"
@@ -12,6 +21,13 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
         onClick={handleLogin}
       />
     </div>
+    <RedirectInfo 
+      text="Need an account? "
+      redirectText="Create an account"
+      additionalStyling={{ marginTop: "5px" }}
+      redirectHandler={handlePushToRegisterPage}
+    />
+    </>
   );
 };
 
